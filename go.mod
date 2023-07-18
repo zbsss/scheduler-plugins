51b23a47e94805d539702ed0fbe835b5c0b72e12
zbsss <michal.kurleto@gmail.com>
2023-07-18 07:58:44 +0000

    querying resources from Device Manager works

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module sigs.k8s.io/scheduler-plugins
 
-go 1.19
+go 1.18
 
 require (
 	github.com/diktyo-io/appgroup-api v1.0.1-alpha
@@ -13,8 +13,10 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/paypal/load-watcher v0.2.3
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.8.0
+	github.com/stretchr/testify v1.8.4
+	github.com/zbsss/device-manager v0.0.2
 	gonum.org/v1/gonum v0.12.0
+	google.golang.org/grpc v1.56.2
 	k8s.io/api v0.26.3
 	k8s.io/apimachinery v0.26.3
 	k8s.io/apiserver v0.26.3
@@ -39,7 +41,7 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -56,7 +58,7 @@ require (
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/cel-go v0.12.6 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
@@ -105,20 +107,19 @@ require (
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/exp v0.0.0-20210220032938-85be41e4509f // indirect
-	golang.org/x/mod v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/term v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.2.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
-	google.golang.org/grpc v1.49.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- pkg/sharedev/sharedev.go
+++ pkg/sharedev/sharedev.go
@@ -2,17 +2,27 @@ package sharedev
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strconv"
+	"time"
 
+	pb "github.com/zbsss/device-manager/generated"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
 // Name is the name of the plugin used in the plugin registry and configurations.
-const Name = "ShareDevPlugin"
+const (
+	Name              = "ShareDevPlugin"
+	deviceManagerPort = "50051"
+)
 
-type ShareDevPlugin struct{}
+type ShareDevPlugin struct {
+}
 
 var _ framework.FilterPlugin = &ShareDevPlugin{}
 
@@ -28,7 +38,78 @@ func (e *ShareDevPlugin) Filter(ctx context.Context, state *framework.CycleState
 		return framework.NewStatus(framework.Unschedulable, "pod does not have sharedev.vendor or sharedev.model label")
 	}
 
+	requests, err := strconv.ParseFloat(pod.Labels["sharedev.requests"], 64)
+	if err != nil {
+		return framework.NewStatus(framework.Unschedulable, "label sharedev.requests is not a float")
+	}
+
+	memory, err := strconv.ParseFloat(pod.Labels["sharedev.memory"], 64)
+	if err != nil {
+		return framework.NewStatus(framework.Unschedulable, "label sharedev.memory is not a float")
+	}
+
+	limits, err := strconv.ParseFloat(pod.Labels["sharedev.limits"], 64)
+	if err != nil {
+		limits = requests
+	}
+
+	var nodeIP string
+	for _, addr := range nodeInfo.Node().Status.Addresses {
+		// TODO: verify this is actually the IP address of the Node
+		if addr.Type == "InternalIP" {
+			nodeIP = addr.Address
+		}
+	}
+
+	log.Println("ShareDevPlugin nodeIP: ", nodeIP)
+
+	fitsDevices, err := e.callDeviceManager(nodeIP, pod.Labels["sharedev.vendor"], pod.Labels["sharedev.model"], requests, limits, memory)
+	if err != nil || len(fitsDevices) == 0 {
+		return framework.NewStatus(framework.Unschedulable, err.Error())
+	}
+
+	log.Println("ShareDevPlugin fitsDevices: ", fitsDevices)
+
+	// TODO: write fitsDevices to state
+	// state.Write(...)
+
+	// TODO: is the state kept in context of a scheduling of a single Pod?
+
 	return framework.NewStatus(framework.Success)
+}
+
+func (e *ShareDevPlugin) callDeviceManager(nodeIP, vendor, model string, requests, limits, memory float64) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
+		fmt.Sprintf("%s:%s", nodeIP, deviceManagerPort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	grpc := pb.NewDeviceManagerClient(conn)
+
+	resp, err := grpc.GetAvailableDevices(ctx, &pb.GetAvailableDevicesRequest{
+		Vendor: vendor,
+		Model:  model,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fitsDevices := []string{}
+	for _, free := range resp.Free {
+		if memory <= free.Memory && requests <= free.Requests {
+			fitsDevices = append(fitsDevices, free.DeviceId)
+		}
+	}
+
+	return fitsDevices, nil
 }
 
 func New(_ runtime.Object, _ framework.Handle) (framework.Plugin, error) {
